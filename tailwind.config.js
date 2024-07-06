f17fdee88f9e6a890fc7854de2e7100e1e7dd30b
Md Zihadur Rahaman <zihadrahman3@gmail.com>
2024-07-06 23:22:33 +0600

    running...

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,11 @@ module.exports = {
       colors: {
       blueColor: '#5046E5',
       hoverBg: '#F4F2FD',
+      editBg: '#F9F8FE',
+      inputFieldBg: '#F0F1F3',
       fontColor: '#1A242C',
       borderColor: '#D9D9D9',      
+      buttonBgColor: '#F15F1D',      
     },   
   },
   },
